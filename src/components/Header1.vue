ea9ab6f82f1f6ee9195ccf5b72b87d5111b745b2
alaahahi <alaa.hahe@gmail.com>
2022-12-02 03:49:48 -0800

    Update Header1.vue

--- src/components/Header1.vue
+++ src/components/Header1.vue
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active text-white" aria-current="page" href="#">مشفى العالمي الأهلي</a>
+          <a class="nav-link active text-white" aria-current="page" href="#">مشفى البشارة الأهلي</a>
         </li>
     
       </ul>
