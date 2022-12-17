f3dfbc2f73a04cfe5253b2f4d61d2f435969981f
alaahahi <alaa.hahe@gmail.com>
2022-12-17 22:48:50 +0300

    fix

--- src/components/Header1.vue
+++ src/components/Header1.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand text-white" href="#">CMS
+    <a class="navbar-brand text-white" href="#">CMSx
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
